db9850e6e9af1035c3c98892e332c7b45327f1f8
TihomirPet <tihomirpetrov@web.de>
2022-03-24 10:24:15 +0100

    delete folder whisky conifere

--- Conifere/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-	
-		anchors:['firstSection', 'secondSection', 'thirdSection','forthSection','fifthSection','sixthSection','seventhSection','eightSection'],
-		menu: '.main-nav ul',
-		fadingEffect: true,
-	
-	});
-});
